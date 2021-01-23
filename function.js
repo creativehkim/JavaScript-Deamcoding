73e86fc4921e471e4c1b44e2e2fb6a8206f8457b
creativehkim <69276681+creativehkim@users.noreply.github.com>
2021-01-23 17:26:33 -0500

    Function notes added

---
+++ function.js
@@ -0,0 +1,155 @@
+// Function
+// - fundamental building block in the program
+// - subprogram can be used multiple times
+// - performs a task or calculates a value
+
+// 1. Function declaration
+// function name(param1, param2) { body... return; }
+// one function === one thing
+// naming: doSomething, command, verb
+// e.g. createCardAndPoint -> createCard, createPoint
+// function is object in JS
+function printHello() {
+  console.log('Hello');
+}
+
+printHello();
+
+function log(message) {
+  console.log(message);
+}
+
+log('Hello');
+log(1234); // type doesn't really matter. it can complicate things. TS needs to say what type it is.
+
+// 2. Parameters
+// premitive parameters: passed by value
+// object parameters: passed by reference
+function changeName(obj) {
+  obj.name = 'coder';
+}
+const hyunji = { name: 'hyunji' };
+changeName(hyunji);
+console.log(hyunji);
+
+// 3. Default parameters (added in ES6)
+function showMessage(message, from = 'unknown') {
+  // now you can set a default value like this
+  console.log(`${message} by ${from}`);
+}
+showMessage('Hi!');
+
+// 4. Rest parameters (added in ES6)
+// '...something' will be an array
+function printAll(...args) {
+  for (let i = 0; i < args.length; i++) {
+    console.log(args[i]);
+  }
+
+  // for of loop
+  for (const arg of args) {
+    console.log(arg);
+  }
+
+  // forEach for arrays
+  args.forEach((arg) => console.log(arg));
+}
+printAll('dream', 'coding', 'hyunji');
+
+// 5. Local scope
+// "You can't see inside from outside but you can see outside from inside "
+let globalMessage = 'global'; // global variable
+function printMessage() {
+  let message = 'hello';
+  console.log(message); // local variable
+  console.log(globalMessage);
+  function printAnother() {
+    console.log(message);
+    let chileMessage = 'hello hello';
+  }
+  // console.log(chileMessage); // error
+}
+printMessage();
+
+// 6. Return a value (if there's no return value, "return undefined" is omitted)
+function sum(a, b) {
+  return a + b;
+}
+const result = sum(1, 2); // 3
+console.log(`sum: ${sum(1, 2)}`);
+
+// 7. Early return, early exit
+// bad
+function upgradeUser(user) {
+  if (user.point > 10) {
+    // long upgrade logic...
+  }
+}
+
+// good
+function upgradeUser(user) {
+  if (user.point <= 10) {
+    return;
+  }
+  // long upgrade logic...
+}
+
+// First-class function
+// functions are treated like any other variables
+// can be assigned as a value to a variable
+// can be passed as an argument to another function
+// can be returned by another function
+
+// 1. Function expression
+// a function declaration can be called earlier than it is defined. (hoisted)
+// a function expression is created when the execution reaches it.
+const print = function () {
+  // anonymous function
+  console.log('print');
+};
+print();
+const printAgain = print;
+printAgain();
+const sumAgain = sum;
+console.log(sumAgain(1, 4));
+
+// 2. Callback function using function expression
+function randomQuiz(answer, printYes, printNo) {
+  if (answer === 'love you') {
+    printYes();
+  } else {
+    printNo();
+  }
+}
+// anonymous function
+const printYes = function () {
+  console.log('Yes!');
+};
+
+// named function
+// better debugging in debugger's stack traces
+// recursions
+const printNo = function print() {
+  console.log('No!');
+  // print(); // recursion
+};
+randomQuiz('wrong', printYes, printNo);
+randomQuiz('love you', printYes, printNo);
+
+// Arrow function
+// always anonymous
+// const simplePrint = function () {
+//   console.log('simplePrint');
+// };
+
+const simplePrint = () => console.log('simplePrint');
+const add = (a, b) => a + b;
+const simpleMultiply = (a, b) => {
+  // do something more
+  return a * b;
+};
+
+// IIFE: Immediately Invoked function expression
+(function hello() {
+  console.log('IIFE');
+})();
