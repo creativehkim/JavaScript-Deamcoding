316929ab1559e264e2f20dda787efa930dbc8bb2
creativehkim <69276681+creativehkim@users.noreply.github.com>
2021-01-25 22:25:06 -0500

    New folder with new js file added

---
+++ async/callback.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// JavaScript is synchronous.
+// Execute the code block in order after hoisting.
+// hoisting: var, function declaration
+console.log('1'); // synchronous
+setTimeout(() => console.log('2'), 1000); // asynchronous
+console.log('3'); // synchronous
+
+// Synchronous callback
+function printImmediately(print) {
+  print();
+} // this function declaration is hoisted(to the top)
+
+printImmediately(() => console.log('hello')); // synchronous
+
+// Asynchronous callback
+function printWithDelay(print, timeout) {
+  setTimeout(print, timeout);
+} // this function declaration is also hoisted(to the top)
+
+printWithDelay(() => console.log('async callback'), 2000); // asynchronous
+
+//Callback Hell example
+class UserStorage {
+  loginUser(id, password, onSuccess, onError) {
+    setTimeout(() => {
+      if (
+        (id === 'ellie' && password === 'dream') ||
+        (id === 'coder' && password === 'academy')
+      ) {
+        onSuccess(id);
+      } else {
+        onError(new Error('not found'));
+      }
+    }, 2000);
+  }
+
+  getRoles(user, onSuccess, onError) {
+    setTimeout(() => {
+      if (user === 'ellie') {
+        onSuccess({ name: 'ellie', role: 'admin' });
+      } else {
+        onError(new Error('no access'));
+      }
+    });
+  }
+}
